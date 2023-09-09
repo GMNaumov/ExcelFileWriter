af2173b3b5406c6225e461c97079519821b70908
Georgii Naumov <gmnaumov@yandex.ru>
2023-09-10 00:22:32 +0300

    feat:initial commit

---
+++ ExcelDataWriter.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using Range = Microsoft.Office.Interop.Excel.Range;
+
+namespace ExcelFileWriter
+{
+    internal class ExcelDataWriter : DataWriter
+    {
+        private readonly Application excelApplication;
+        private readonly object missValue;
+
+        public ExcelDataWriter()
+        {
+            excelApplication = new Application();
+            missValue = System.Reflection.Missing.Value;
+        }
+
+        public void Write(List<object> objects)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Запись в ячейки листа Excel производится присваиванием значению Cell[строка, столбец]
+        /// Нумерация значений начинается со значения 1.
+        /// </summary>
+        /// <param name="cars"></param>
+        public void Write(List<Car> cars, string path)
+        {
+            Workbook excelWorkbook = excelApplication.Workbooks.Add();
+            Worksheet excelWorksheet = excelWorkbook.Worksheets[1];
+
+            try
+            {
+                SetTableHeaders(excelWorksheet);
+                
+                for (int i = 0; i < cars.Count; i++)
+                {
+                    Car next = cars[i];
+
+                    int cellNum = i + 2;
+
+                    excelWorksheet.Cells[cellNum, 1] = next.Brand;
+                    excelWorksheet.Cells[cellNum, 2] = next.Model;
+                    excelWorksheet.Cells[cellNum, 3] = next.Price;
+                }
+
+
+                excelApplication.Visible = false;
+                excelWorkbook.SaveAs2(path);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Exception has raised!");
+                Console.WriteLine(exception.StackTrace);
+            }
+            finally
+            {
+                excelWorkbook.Close();
+                excelApplication.Quit();
+            }
+        }
+
+        /// <summary>
+        /// Устанавливаем заголовки таблицы
+        /// </summary>
+        /// <param name="worksheet"></param>
+        private void SetTableHeaders(Worksheet worksheet)
+        {
+            worksheet.Range["A1"].Value = "Car Brand";
+            worksheet.Range["B1"].Value = "Car Model";
+            worksheet.Range["C1"].Value = "Car Price";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using ExcelFileWriter;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car carOne = new() { Brand = "BMW", Model = "M5", Price = 2_500_000.0 };
+        Car carTwo = new() { Brand = "Mercedes", Model = "E250", Price = 3_700_000.0 };
+        Car carThree = new() { Brand = "Audi", Model = "A6", Price = 2_200_000.0 };
+
+        List<Car> cars = new()
+        {
+            carOne,
+            carTwo,
+            carThree
+        };
+
+        try
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.InitialDirectory = Application.StartupPath;
+            StreamWriter streamWriter;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                streamWriter = new StreamWriter(saveFileDialog.FileName);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.StackTrace);
+        }
+
+        ExcelDataWriter writer = new();
+        string path = @"D:\NewFile.xlsx";
+        writer.Write(cars, path);
+    }
+}
